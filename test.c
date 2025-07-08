a2a7f4ee65c356e66dd9b971cf5fd1389c1dca08
kimyounhoex1 <dordyd11@gmail.com>
2025-07-09 04:41:24 +0900

    aaa-0

--- test.c
+++ test.c
@@ -1,9 +1,2 @@
 #include <stdio.h>
-
-int main(void) {
-  int a = 0;
-  if (a = 5) { // clang-tidy: warning (assign instead of compare)
-    printf("a is 5\\n");
-  }
-  return 0;
-}
+int main(void){printf("Hello, world!\\n");return 0;}
