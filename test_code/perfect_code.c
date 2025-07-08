2bccc84580b8a6f1e4ca41c2ee9c1d0c167df72f
kimyounhoex1 <dordyd11@gmail.com>
2025-07-08 16:48:30 +0900

    ghj-2

--- test_code/perfect_code.c
+++ test_code/perfect_code.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-int main(
-
-) 
-{
-    // int a;
-    printf("hello world!");
-    if(1==1){
-      printf("제밸 디라")
-    }
-    return 0;
+int main() {
+  // int a;
+  printf("hello world!");
+  if (1 == 1) {
+    printf("제밸 디라")
+  }
+  return 0;
 }
