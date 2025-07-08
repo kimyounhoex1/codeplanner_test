692e46bd7474780211d921f6ca7b58f18d8d6635
kimyounhoex1 <dordyd11@gmail.com>
2025-07-09 02:02:42 +0900

    sb-0

--- test_code/perfect_code.c
+++ test_code/perfect_code.c
@@ -2,30 +2,7 @@
 
 int main() {
   // int a;
-  printf("hello world!");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
-  printf("제밸 디라");
+  
   printf("제밸 디라");
   printf("제밸 디라");
   return 0;
