7ceb5250115d7b1da0dc44d6494be1e44b8f18ba
kimyounhoex1 <dordyd11@gmail.com>
2025-07-09 04:32:44 +0900

    aaa-0

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    int a = 0;
-    if (a = 5) { // clang-tidy: warning (assign instead of compare)
-        printf("a is 5\\n");
+  int a = 0;
+  if (a = 5) { // clang-tidy: warning (assign instead of compare)
+    printf("a is 5\\n");
     }
-    return 0;
+  return 0;
 }
