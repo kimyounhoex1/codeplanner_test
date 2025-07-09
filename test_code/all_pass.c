c844f0d8946d659d22a21fadcfd576eaf2a3daeb
kimyounhoex1 <dordyd11@gmail.com>
2025-07-09 11:25:44 +0900

    aaa-0

--- test_code/all_pass.c
+++ test_code/all_pass.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-  for(int i = 0; i< 10; i++){
-    if(i == 5){
+  for (int i = 0; i < 10; i++) {
+    if (i == 5) {
       printf("i is 5\\n");
     }
   }
   return 0;
-
 }
